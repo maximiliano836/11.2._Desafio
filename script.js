63b9f4dd8d6158aa24037f4a0601c4eee85de536
maximiliano836 <maxi10.02.00.g@gmail.com>
2023-09-05 09:35:06 -0300

    fin de la tarea

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let Nombre=document.getElementById("Nombre");
-let Apellido=document.getElementById("Apellido");
-let date=document.getElementById("date");
-let button=document.getElementsById("button");
+const Nombre=document.getElementById("Nombre");
+const Apellido=document.getElementById("Apellido");
+const date=document.getElementById("date");
+const button=document.getElementById("button");
 
-button.addEventListener("click", function(){
-    method: "POST",
-    fetch ("https://jsonplaceholder.typicode.com/users", {
-    body: JSON.stringify({
+button.addEventListener("click", function () {
+    const data = {
         nombre: Nombre.value,
         apellido: Apellido.value,
-        Date: date.value,
+        Fecha_de_Nacimiento: date.value,
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
     })
+    .then(response => response.json())
+    .then(data => {
+        alert("Respuesta del servidor: " + JSON.stringify(data));
+    })
+    .catch(error => {
+        console.error("Error al enviar los datos: " + error);
+    });
 });
-alert("\nStatus: " + status)
-})
